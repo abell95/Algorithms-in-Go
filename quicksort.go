4c89e430e0d197963f55b858721abace6f79eb64
Andrew <33483980+abell95@users.noreply.github.com>
2017-12-23 02:50:00 -0800

    Update quicksort.go

--- quicksort.go
+++ quicksort.go
@@ -9,11 +9,12 @@ import (
 
 func quickSort(numList []int) {
 	//todo
+	//return sorted list
 }
 
 func main() {
 	arr := []int{8, 99, 13, 5, 73, 22, 6, 44, 5, 11}
-	quicksort(arr)
+	sortedArr := quicksort(arr)
 	fmt.Println(arr)
 	if sort.IntsAreSorted(arr) == true {
 		fmt.Println("It's sorted!")
